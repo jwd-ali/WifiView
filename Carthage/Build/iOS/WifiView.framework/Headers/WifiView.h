5a158f2b677475975ba5623ec2afa5efb481c566
Jawad Ali <jawad.ali.ext@foodpanda.com>
2021-09-24 16:53:01 +0500

    carthage support

---
+++ Carthage/Build/iOS/WifiView.framework/Headers/WifiView.h
@@ -0,0 +1,18 @@
+//
+//  WifiView.h
+//  WifiView
+//
+//  Created by Jawad Ali on 24/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WifiView.
+FOUNDATION_EXPORT double WifiViewVersionNumber;
+
+//! Project version string for WifiView.
+FOUNDATION_EXPORT const unsigned char WifiViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WifiView/PublicHeader.h>
+
+
